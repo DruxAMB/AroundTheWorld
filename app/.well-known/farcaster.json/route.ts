16e1f717752f6c5a2cf96cef7cf9de60cb9457fd
DruxAMB <druxambmarketing@gmail.com>
2025-08-14 10:24:33 +0100

    added baseBuilder property to farcaster.json to help sync with baseapp

--- app/.well-known/farcaster.json/route.ts
+++ app/.well-known/farcaster.json/route.ts
@@ -39,5 +39,8 @@ export async function GET() {
       ogDescription: process.env.NEXT_PUBLIC_APP_OG_DESCRIPTION,
       ogImageUrl: process.env.NEXT_PUBLIC_APP_OG_IMAGE,
     }),
+    "baseBuilder": {
+    "allowedAddresses": ["0xe5A519177EAcDeC56A44EED8696cF8288F0Ec4EA", "0x78267DF474bF819B7Ab56A854C24f7A417Ae6371"]
+  }
   });
 }
